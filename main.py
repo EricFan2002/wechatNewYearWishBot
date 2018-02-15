737cf0e57842888c9b4b87392d676cca86b144de
Eric <fty20020322@gmail.com>
2018-02-15 22:08:51 +0800

    added loop to avvoid logout

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ from itchat.content import *
 
 
 
-@itchat.msg_register(itchat.content.TEXT)
+@itchat.msg_register(TEXT)
 def text_reply(msg):
     msg.user.send("%s 祝你也新年快乐哦LoL" % msg.user["NickName"])
 
 
 
 itchat.auto_login()
+
 friendList = itchat.get_friends(update=True)[1:]
 for aFriend in friendList:
-    aFriend.send("Hey %s! 祝你新年快乐哦~ \r\n(By Eric's Bot@https://github.com/EricFan2002/wechatNewYearWishBot)" % aFriend["NickName"])
+    aFriend.send("Hey %s! 祝你新年快乐哦~ \r\n(By Eric's Bot@https://github.com/EricFan2002/wechatNewYearWishBot )" % aFriend["NickName"])
     time.sleep(1)
 
+itchat.run(True)
 
